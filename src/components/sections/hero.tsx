c432c24dbf58dda0c085266695272d2e1c5d70f9
Santosh Kumar Jena <santoshk.jena@hyscaler.com>
2025-07-09 23:54:54 +0530

    updated the cv

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -83,7 +83,7 @@ const HeroSection = () => {
               <div className="mt-8 md:ml-2 flex flex-col gap-3">
                 <Link
                   href={
-                    "https://drive.google.com/file/d/1O97WCk2DrO9x6SHOqf7LvRbmHkMgGIb4/view?usp=sharing"
+                    "https://drive.google.com/drive/folders/1ORjIAIujCUfb1vpiDch4VBvbgLYFbZ7Z"
                   }
                   target="_blank"
                   className="flex-1"
